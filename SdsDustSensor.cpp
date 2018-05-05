c456dffbce7ac6b75180f849ce9810072fa4bc63
lewapek <lewapek@gmail.com>
2018-05-05 23:34:38 +0200

    responses abstraction added + refactor

---
+++ SdsDustSensor.cpp
@@ -0,0 +1,96 @@
+#include "SdsDustSensor.h"
+
+const char *SdsDustSensor::statusToString(Status *status) {
+  switch (*status) {
+    case Status::Ok: return "Ok";
+    case Status::NotAvailable: return "Not available";
+    case Status::InvalidChecksum: return "Invalid checksum";
+    case Status::InvalidResponseId: return "Invalid response id";
+    case Status::InvalidHead: return "Invalid head";
+    case Status::InvalidTail: return "Invalid tail";
+    default: return "undefined status";
+  }
+}
+
+void SdsDustSensor::write(const Command *command) {
+  sdsSerial->flush();
+  for (int i = 0; i < Command::length; ++i) {
+    sdsSerial->write(command->bytes[i]);
+    #ifdef __DEBUG_SDS_DUST_SENSOR__
+    Serial.print("|");
+    Serial.print(command->bytes[i], HEX);
+    #endif
+  }
+  #ifdef __DEBUG_SDS_DUST_SENSOR__
+  Serial.println("| <- written bytes");
+  #endif
+  delay(500);
+}
+
+Status SdsDustSensor::readIntoBytes(byte responseId) {
+  int checksum = 0;
+  int readBytesQuantity = 0;
+
+  while ((sdsSerial->available() > 0) &&
+        (sdsSerial->available() >= Result::lenght - readBytesQuantity)) {
+    byte readByte = sdsSerial->read();
+    response[readBytesQuantity] = readByte;
+
+    #ifdef __DEBUG_SDS_DUST_SENSOR__
+    Serial.print("|");
+    Serial.print(readByte, HEX);
+    #endif
+
+    ++readBytesQuantity;
+    switch (readBytesQuantity) {
+      case 1:
+        if (readByte != 0xAA) {
+          #ifdef __DEBUG_SDS_DUST_SENSOR__
+          Serial.println("| <- read bytes with invalid head error");
+          #endif
+          return Status::InvalidHead;
+        }
+        break;
+      case 2:
+        if (readByte != responseId) {
+          #ifdef __DEBUG_SDS_DUST_SENSOR__
+          Serial.println("| <- read bytes with invalid response id");
+          #endif
+          return Status::InvalidResponseId;
+        }
+        break;
+      case 3 ... 8:
+        checksum += readByte;
+        break;
+      case 9:
+        if (readByte != checksum % 256) {
+          #ifdef __DEBUG_SDS_DUST_SENSOR__
+          Serial.println("| <- read bytes with invalid checksum");
+          #endif
+          return Status::InvalidChecksum;
+        }
+        break;
+      case 10:
+        if (readByte != 0xAB) {
+          #ifdef __DEBUG_SDS_DUST_SENSOR__
+          Serial.println("| <- read bytes with invalid tail");
+          #endif
+          return Status::InvalidTail;
+        }
+        break;
+    }
+    if (readBytesQuantity == Result::lenght) {
+      #ifdef __DEBUG_SDS_DUST_SENSOR__
+      Serial.println("| <- read bytes with success");
+      #endif
+      return Status::Ok;
+    }
+    yield();
+  }
+
+  #ifdef __DEBUG_SDS_DUST_SENSOR__
+  Serial.println("| <- read bytes with no more available");
+  #endif
+
+  return Status::NotAvailable;
+}
---
+++ SdsDustSensor.h
@@ -0,0 +1,119 @@
+#ifndef __SDS_DUST_SENSOR_H__
+#define __SDS_DUST_SENSOR_H__
+
+#include "SdsDustSensorCommands.h"
+#include "SdsDustSensorResults.h"
+#include <SoftwareSerial.h>
+
+// #define __DEBUG_SDS_DUST_SENSOR__
+
+class SdsDustSensor {
+public:
+  SdsDustSensor(int pinRx, int pinTx, int baudRate = 9600):
+    sdsSerial(new SoftwareSerial(pinRx, pinTx)),
+    baudRate(baudRate) {}
+
+  ~SdsDustSensor() {
+    if (sdsSerial != NULL) {
+      delete sdsSerial;
+    }
+  }
+
+  const char *statusToString(Status status) {
+    return statusToString(&status);
+  }
+
+  const char *statusToString(Status *status);
+
+  void begin() {
+    sdsSerial->begin(baudRate);
+  }
+
+  byte *getLastResponse() {
+    return response;
+  }
+
+  ReportingModeResult queryReportingMode() {
+    Status status = execute(&Commands::queryReportingMode);
+    return ReportingModeResult(status, response);
+  }
+
+  ReportingModeResult setQueryReportingMode() {
+    Status status = execute(&Commands::setQueryReportingMode);
+    return ReportingModeResult(status, response);
+  }
+
+  ReportingModeResult setActiveReportingMode() {
+    Status status = execute(&Commands::setActiveReportingMode);
+    return ReportingModeResult(status, response);
+  }
+
+  PmResult queryPm() {
+    Status status = execute(&Commands::queryPm);
+    return PmResult(status, response);
+  }
+
+  // warning: this method doesn't write anything to the sensor, it just reads incoming bytes
+  PmResult readPm() {
+    Status status = readIntoBytes(Commands::queryPm.responseId);
+    return PmResult(status, response);
+  }
+
+  Result setDeviceId(byte newDeviceIdByte1, byte newDeviceIdByte2) {
+    (Commands::setDeviceId).setDeviceId(newDeviceIdByte1, newDeviceIdByte2);
+    Status status = execute(&Commands::setDeviceId);
+    return Result(status, response);
+  }
+
+  WorkingStateResult queryWorkingState() {
+    Status status = execute(&Commands::queryWorkingState);
+    return WorkingStateResult(status, response);
+  }
+
+  WorkingStateResult sleep() {
+    Status status = execute(&Commands::sleep);
+    return WorkingStateResult(status, response);
+  }
+
+  WorkingStateResult wakeup() {
+    Status status = execute(&Commands::wakeup);
+    return WorkingStateResult(status, response);
+  }
+
+  WorkingPeriodResult queryWorkingPeriod() {
+    Status status = execute(&Commands::queryWorkingPeriod);
+    return WorkingPeriodResult(status, response);
+  }
+
+  WorkingPeriodResult setContinuousWorkingPeriod() {
+    (Commands::setWorkingPeriod).setContinuousWorkingPeriod();
+    Status status = execute(&Commands::setWorkingPeriod);
+    return WorkingPeriodResult(status, response);
+  }
+
+  WorkingPeriodResult setCustomWorkingPeriod(byte workingPeriod) {
+    (Commands::setWorkingPeriod).setCustomWorkingPeriod(workingPeriod);
+    Status status = execute(&Commands::setWorkingPeriod);
+    return WorkingPeriodResult(status, response);
+  }
+
+  FirmwareVersionResult queryFirmwareVersion() {
+    Status status = execute(&Commands::queryFirmwareVersion);
+    return FirmwareVersionResult(status, response);
+  }
+
+  Status execute(const Command *command) {
+    write(command);
+    return readIntoBytes(command->responseId);
+  }
+
+  void write(const Command *command);
+  Status readIntoBytes(byte responseId);
+
+private:
+  byte response[Result::lenght];
+  int baudRate;
+  SoftwareSerial *sdsSerial = NULL;
+};
+
+#endif // __SDS_DUST_SENSOR_H__
